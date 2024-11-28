04f928869f7d781f29ecd9f6cce7c74ff18f3a05
kriti <kritishrivastav17@gmail.com>
2024-11-28 13:52:32 +0530

    added netlify.toml file

--- client/src/pages/AddNewProblemPage.jsx
+++ client/src/pages/AddNewProblemPage.jsx
@@ -36,7 +36,7 @@ const AddNewProblemPage = () => {
   };
 
   return (
-    <section className="bg-gray-100">
+    <section className="bg-gray-700 border-t">
       <div className="container m-auto max-w-2xl py-24">
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
